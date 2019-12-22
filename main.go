c0fdd59f2acd7eb92bc3dd107c9f8cd39af9dbef
bruno edoh <edohbruno@gmail.com>
2019-12-22 23:22:37 +0000

    experimenting with methods

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type Node struct {
+  data string
+  next *Node
+}
+
+func (v Node) printData() string {
+  return v.data;
+}
+
 func main() {
-  fmt.Println("Hello World")
+  node := Node{"bruno", nil}
+  fmt.Println(node.printData())
 }

07af249daacb57b5e5152902793f7c58a4e43bee
bruno edoh <edohbruno@gmail.com>
2019-12-30 11:52:24 +0000

    constant, iota

--- main.go
+++ main.go
@@ -1,44 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Node struct {
-  data string
-  next *Node
-}
-
-type Stringly string
-
-func (v Stringly) printData()  {
-  fmt.Println(">>>>>>>>>>", v)
-}
-
-type Human struct {
-  name string
-  age uint16
-}
-
-func (human *Human) setInfo(name string, age uint16) {
-  human.name = name
-  human.age = age
-}
-
-type walkable interface {
-  kind() string
-  distance() float64
-}
-
-func (vv Human) kind() string {
-  return "I am kinda kind"
-}
-
-func (vv Human) distance() float64 {
-  return 14.23
-}
+ 
 
 func main() {
-  bruno := &Human{"kofi", 39} 
-  fmt.Println(bruno.distance())
-  fmt.Println(bruno.kind())
+	const factor = iota
+  fmt.Printf("%v---%T", factor)
 }
-

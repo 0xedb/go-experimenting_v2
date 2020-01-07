c1bc220e312185fb365dd0353837ec88c79a1639
bruno edoh <edohbruno@gmail.com>
2020-01-07 02:20:17 +0000

    goroutine + waitgroup

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
-  "runtime"
+	"sync"
 )
 
- 
+func writeSomething(msg string, num int, wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		for i := 0; i < num; i++ {
+			fmt.Println(msg)
+		}
+		wg.Done()
+	}()
+}
 
-func main() { 
-	var mm  map[int]string
-  if mm == nil {
-    fmt.Println("mm is nil")
-  }
-  fmt.Println(mm)
-  fmt.Println(runtime.NumCPU())
+func main() {
+	var wg sync.WaitGroup
+  writeSomething("bruno", 10, &wg)
+  writeSomething("kofi", 20, &wg)
+  writeSomething("edoh", 14, &wg)
+	wg.Wait()
 }

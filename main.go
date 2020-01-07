36959f29ed77af984d6090658725de8103082b60
bruno edoh <edohbruno@gmail.com>
2020-01-07 02:01:31 +0000

    show number of logical CPUs

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+  "runtime"
 )
 
  
 
-func main() {
+func main() { 
 	var mm  map[int]string
   if mm == nil {
     fmt.Println("mm is nil")
   }
   fmt.Println(mm)
+  fmt.Println(runtime.NumCPU())
 }
